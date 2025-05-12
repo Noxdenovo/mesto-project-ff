26df2af1be3e47624701284f6f6540992922125e
Petr <verkhovp@gmail.com>
2025-05-12 17:06:45 +0300

    finished the project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function deleteCard(evt) {
+  const listItem = evt.target.closest(".places__item");
+  listItem.remove();
+}
 
-// @todo: Функция создания карточки
+function addCard(initialCard, deleteFunction) {
+  const newCard = cardTemplate.querySelector(".card").cloneNode(true);
+  newCard.querySelector(".card__image").src = initialCard.link;
+  newCard.querySelector(".card__title").textContent = initialCard.name;
+  const deleteButton = newCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", (evt) => deleteFunction(evt));
+  return newCard;
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  placesList.append(addCard(element, deleteCard));
+});
